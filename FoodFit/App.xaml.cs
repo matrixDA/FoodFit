774316df651507fe1977018bc6230993b797caa7
Dinesh <dadhikari03@protonmail.com>
2025-04-04 10:19:02 -0400

    changed app xaml cs page

--- FoodFit/App.xaml.cs
+++ FoodFit/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace FoodFit
+﻿using FoodFit.Views;
+
+namespace FoodFit
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new SignUpPage());
         }
     }
 }
