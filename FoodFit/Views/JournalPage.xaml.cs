9addcc5dad60921e3d8656d212dbea922624a268
Ravi Nayak <nayakrr@mail.uc.edu>
2025-04-09 11:26:20 -0400

    adding journal and journal entry

---
+++ FoodFit/Views/JournalPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace FoodFit.Views;
+
+public partial class JournalPage : ContentPage
+{
+	public JournalPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+    {
+		//Add logic for button click to add journal logs
+    }
+}
