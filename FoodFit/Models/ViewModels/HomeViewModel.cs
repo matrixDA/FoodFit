765911904d6431ed8d71aab38645f14a1557a559
Ravi Nayak <nayakrr@mail.uc.edu>
2025-04-09 09:59:16 -0400

    got rid of categories

--- FoodFit/Models/ViewModels/HomeViewModel.cs
+++ FoodFit/Models/ViewModels/HomeViewModel.cs
@@ -5,35 +5,12 @@ namespace FoodFit.ViewModels
 {
     public partial class HomeViewModel : ObservableObject
     {
-        public ObservableCollection<string> FoodCategories { get; } = new()
-        {
-            "Fruits", "Vegetables", "Dairy", "Meat", "Grains", "Seafood", "Beverages", "Snacks"
-        };
+
 
         [ObservableProperty]
         private ObservableCollection<string> filteredCategories;
 
         [ObservableProperty]
         private string searchText;
-
-        public HomeViewModel()
-        {
-            FilteredCategories = new ObservableCollection<string>(FoodCategories);
-        }
-
-        partial void OnSearchTextChanged(string value)
-        {
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                FilteredCategories = new ObservableCollection<string>(FoodCategories);
-            }
-            else
-            {
-                var filtered = FoodCategories
-                    .Where(c => c.ToLower().Contains(value.ToLower()))
-                    .ToList();
-                FilteredCategories = new ObservableCollection<string>(filtered);
-            }
         }
     }
-}
