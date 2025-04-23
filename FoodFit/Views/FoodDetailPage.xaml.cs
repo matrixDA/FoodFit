0b8ec1922b49830448e606b62f43673f973ad7ff
Ravi Nayak <nayakrr@mail.uc.edu>
2025-04-23 11:26:41 -0400

    added code and backend logic for nutrient page

--- FoodFit/Views/FoodDetailPage.xaml.cs
+++ FoodFit/Views/FoodDetailPage.xaml.cs
@@ -12,4 +12,8 @@ public partial class FoodDetailPage : ContentPage
         SelectedFood = food;
         BindingContext = this;
     }
+    private async void AddToLogButton_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Added to Log", $"{SelectedFood.FoodName} has been added to your log.", "OK");
+    }
 }
